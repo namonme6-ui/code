fd516944ac18a7e4b79346560921041ac46c98b7
namon <namon.me6@gmail.com>
2025-11-26 22:13:56 +1300

    feat: code exercise

---
+++ beginner/product.py
@@ -0,0 +1,8 @@
+n1 = int(input("enter number 1: "))
+n2 = int(input("enter number 2: "))
+
+if n1*n2 <= 1000:
+    print("The result is:",n1*n2)
+else:
+    print("The result is:",n1+n2)
+    
---
+++ leetcode/two_sum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+# [2,3,4,5]
+# 7 -> [1,2]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+
+        return []
+
+
